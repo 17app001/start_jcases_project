977659aab946a09d5730021b83566057896a3da1
jerry <17app001@gmail.com>
2022-01-10 10:53:59 +0800

    初始models

--- case/admin.py
+++ case/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from .models import City
+from .models import City,Category,Amount,Mode,State,Respondent,Period
 
 # Register your models here.
 admin.site.register(City)
+admin.site.register(Category)
+admin.site.register(Amount)
+admin.site.register(Mode)
+admin.site.register(State)
+admin.site.register(Respondent)
+admin.site.register(Period)
+
--- case/models.py
+++ case/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 # Create your models here.
+# 縣市
 class City(models.Model):
     name = models.CharField(max_length=50, unique=True, null=False)
     createdon = models.DateField(auto_now_add=True)
@@ -11,3 +12,69 @@ class City(models.Model):
     def __str__(self):
         return f"{self.id}-{self.name}-{self.createdon}"
 
+# 分類
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True, null=False)
+    createdon = models.DateField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["id"]
+
+    def __str__(self):
+        return f"{self.id}-{self.name}-{self.createdon}"
+
+# 外包金額
+class Amount(models.Model):
+    name = models.CharField(max_length=50, unique=True, null=False)
+    createdon = models.DateField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["id"]
+
+    def __str__(self):
+        return f"{self.id}-{self.name}-{self.createdon}"
+
+# 配合模式
+class Mode(models.Model):
+    name = models.CharField(max_length=50, unique=True, null=False)
+    createdon = models.DateField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["id"]
+
+    def __str__(self):
+        return f"{self.id}-{self.name}-{self.createdon}"
+
+# 目前狀態
+class State(models.Model):
+    name = models.CharField(max_length=50, unique=True, null=False)
+    createdon = models.DateField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["id"]
+
+    def __str__(self):
+        return f"{self.id}-{self.name}-{self.createdon}"
+
+# 工作週期
+class Period(models.Model):
+    name = models.CharField(max_length=50, unique=True, null=False)
+    createdon = models.DateField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["id"]
+
+    def __str__(self):
+        return f"{self.id}-{self.name}-{self.createdon}"
+
+
+# 接案身分
+class Respondent(models.Model):
+    name = models.CharField(max_length=50, unique=True, null=False)
+    createdon = models.DateField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["id"]
+
+    def __str__(self):
+        return f"{self.id}-{self.name}-{self.createdon}"
