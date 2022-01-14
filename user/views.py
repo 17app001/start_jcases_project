b486f2e578b520d57ce441874320425debe79ecb
jerry <jerry@gmail.com>
2022-01-14 20:55:48 +0800

    1.add base.html 2.增加輸入錯誤資訊 3.登入錯誤資訊顯示 4.準備註冊畫面跟message module

--- user/views.py
+++ user/views.py
@@ -1,6 +1,9 @@
 from cmath import log
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+# from django.contrib.auth.models import User
+from user.models import Profile
+
 # Create your views here.
 
 # 登出
@@ -15,23 +18,27 @@ def user_logout(request):
 
 # 登入
 def user_login(request):
+
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
-
-        print(username, password)
-
         try:
             user = authenticate(request, username=username, password=password)
 
             if user:
                 login(request, user)
-
                 return redirect('cases')
             else:
-                print('登入失敗!')
+                if Profile.objects.filter(username=username).exists():
+                    message = '密碼錯誤'
+                else:
+                    message = '帳號錯誤'
+
+                print(message)
 
         except Exception as e:
             print(e)
+
+        return render(request, './user/login.html', {'username': username, 'password': password, 'message': message})
 
     return render(request, './user/login.html')
