d017f1faf2ac09f8d417ad109b151e76104764fc
jerry <jerry@gmail.com>
2022-01-25 21:59:28 +0800

    1.使用include tempate 2.個人資訊分頁功能 3.個人資訊修改功能 4.新增認證信件功能

--- user/urls.py
+++ user/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('logout/', views.user_logout, name='logout'),
     path('register/', views.user_register, name='register'),
     path('profile/<str:id>', views.profile, name='profile'),
-
+    path('update/<str:id>',views.update,name='update'),
+    path('activate-email/', views.activate_email,name='activate-email'),
+      path('activate-user/<uidb64>/<token>/',views.user_activate, name='activate'),
 ]
--- user/views.py
+++ user/views.py
@@ -2,19 +2,105 @@ from cmath import log
 from email import message
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from user.models import Profile
+from user.models import Profile,Respondent
 from user.forms import ProfileForm
 from django.contrib.auth.decorators import login_required
+from case.utils import get_page_object
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode,urlsafe_base64_decode
+from django.utils.encoding import force_bytes,force_str
+from django.core.mail import EmailMessage
+from django.conf import settings
+from .utils import generator_token
+
+
+
+# 啟動玩家
+def user_activate(requset, uidb64, token):
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = Profile.objects.get(id=uid)
+
+    except Exception as e:
+        user = None
+
+    # 檢查是否認證成功
+    if user and generator_token.check_token(user, token):
+        user.certification = True
+        user.point += 100
+        user.save()    
+    
+
+    return redirect('profile', user.id)
 
 # Create your views here.
+@login_required(login_url='login')
+def activate_email(request):
+    user=request.user
+    domain=get_current_site(request)
+    try:
+        email_subject='Activate your account'
+        email_body=render_to_string('./user/activate.html',{
+            'user':user,
+            'domain':domain,
+            'uid':urlsafe_base64_encode(force_bytes(user.id)),
+            'token':generator_token.make_token(user),
+        })
 
-# 個人資訊
+        email = EmailMessage(
+            subject=email_subject,
+            body=email_body,
+            from_email=settings.EMAIL_HOST_USER,
+            to=[user.email],
+        )
+
+        email.send()
+        message='認證EMAIL發送...'
+
+    except Exception as e:
+        print(e)
+        message='認證EMAIL發送失敗...'        
+
+    return render(request,'./user/profile.html',{'user':user,'message':message})
 
 
 @login_required(login_url='login')
+def update(request,id):   
+    respondents=Respondent.objects.all()
+    user = Profile.objects.get(id=id)
+    message=None
+
+    if request.method=='POST':       
+        new_email=request.POST.get('new-email')       
+        respondent_id=request.POST.get('respondent-id')       
+
+        if not new_email:
+            message='請輸入Email'                 
+        elif not Profile.objects.filter(email=new_email) or user.email==new_email:
+            setattr(user, 'email', new_email)          
+            setattr(user,'respondent',Respondent.objects.get(id=respondent_id))
+            message='資料更新成功!'           
+            user.save()
+
+            return redirect('profile', id=user.id)
+        else:         
+            message='Email已經註冊'            
+
+        
+    return render(request, './user/update.html',{'message':message,'respondents':respondents})
+
+
+# 個人資訊
+@login_required(login_url='login')
 def profile(request, id):
     user = Profile.objects.get(id=id)
-    respsone=render(request, './user/profile.html', {'user': user})
+    cases=user.case_set.all()
+    page = request.GET.get('page')
+    page_num=10
+    page_obj = get_page_object(cases, page, page_num)
+
+    respsone=render(request, './user/profile.html', {'user': user,'page_obj':page_obj})
     respsone.set_cookie('page','profile') 
 
     return respsone
