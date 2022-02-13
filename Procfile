a5115fb4b40be186e482838ba9ba1d7e213d7b83
jerry <jerry@gmail.com>
2022-02-13 21:31:15 +0800

    新增create-case/update-case

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn jcases.wsgi
--- case/forms.py
+++ case/forms.py
@@ -14,3 +14,23 @@ class CreateCaseForm(ModelForm):
             'respondent': forms.CheckboxSelectMultiple(),
             'mode': forms.CheckboxSelectMultiple(),
         }
+
+    def __init__(self, *args, **kwargs):
+        super(CreateCaseForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.help_text = None
+
+            if name in ['category', 'amount', 'period', 'state']:
+                field.widget.attrs.update(
+                    {'class': 'select is-size-5 mb-2'})
+            elif name == 'description':
+                field.widget.attrs.update(
+                    {'class': 'textarea is-size-6 mb-2'})
+            elif name in ['respondent', 'mode']:
+                field.widget.attrs.update(
+                    {'class': 'checkbox is-size-5 mb-2'})
+            else:
+                field.widget.attrs.update(
+                    {'class': 'input label is-size-5 has-text-centered mt-2 mb-2'}
+                )
